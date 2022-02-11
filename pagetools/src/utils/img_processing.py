5209ea1d0c9baacaad33f15ff29d15ffd17f086f
JKamlah <jan.kamlah@gmail.com>
2022-02-11 11:25:08 +0100

    Rework: Image rotation now per default it will return a image which contains all image informations (change of image size), but you can also use the older version instead.

--- pagetools/src/utils/img_processing.py
+++ pagetools/src/utils/img_processing.py
@@ -44,19 +44,36 @@ def calc_median_color(img: np.array) -> np.ndarray:
     return np.median(np.median(img, axis=0), axis=0)
 
 
-def rotate_img(image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]]) -> np.ndarray:
+def rotate_img(image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]], display_all: bool = True) \
+        -> np.ndarray:
     """
-
     :param image:
     :param angle:
     :param background:
     :return:
     """
-    rows = image.shape[0]
-    cols = image.shape[1]
-
-    img_center = (cols / 2, rows / 2)
-    rot_mat = cv2.getRotationMatrix2D(img_center, angle*-1, 1)
-
-    rotated_image = cv2.warpAffine(image, rot_mat, (cols, rows), borderValue=background)
-    return rotated_image
+    # author:   Adrian Rosebrock
+    # website:  http://www.pyimagesearch.com
+    # source:   https://github.com/PyImageSearch/imutils/convenience.py
+    # license:  MIT
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    if display_all:
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+        # perform the actual rotation and return the image
+        # compute the new bounding dimensions of the image
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+        # adjust the rotation matrix to take into account translation
+        M[0, 2] += (nW / 2) - cX
+        M[1, 2] += (nH / 2) - cY
+        return cv2.warpAffine(image, M, (nW, nH), borderValue=background)
+    else:
+        return cv2.warpAffine(image, M, (h, w), borderValue=background)
